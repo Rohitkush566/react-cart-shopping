eb924feed43db37c55d064af0dd1f39103178339
Rohit Kushwah <112879238+Rohitkush566@users.noreply.github.com>
2023-11-06 00:24:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyBMLwvVDvrQLSQrT2aHakpsPIX6CbVPzLQ",
+  authDomain: "auth-822f2.firebaseapp.com",
+  projectId: "auth-822f2",
+  storageBucket: "auth-822f2.appspot.com",
+  messagingSenderId: "705317315346",
+  appId: "1:705317315346:web:15f6c5abcf6645000fc9cb"
 };
 
 const app = initializeApp(firebaseConfig);
